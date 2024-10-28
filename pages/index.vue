5a7dcf3a7b358220611a95df369429166616685f
Phillip Matambanadzo <hello@philmataz.dev>
2024-10-28 10:22:37 +0000

    feat(i18n): add internationalization support
    Implement basic i18n functionality using @nuxtjs/i18n module. Add language
    switcher buttons to the home page allowing users to toggle between English
    and French translations.
    
    The addition of i18n support is a first step toward making the application
    accessible to a broader, multilingual audience.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,23 @@
-<script setup lang="ts"></script>
+<script setup>
+const { setLocale } = useI18n();
+</script>
 
 <template>
-  <div>Hello Home!</div>
+  <div>
+    <div class="flex gap-2">
+      <button
+        class="inline-flex rounded bg-teal-500 px-4 py-2 text-white"
+        @click="setLocale('en')"
+      >
+        en
+      </button>
+      <button
+        class="inline-flex rounded bg-teal-500 px-4 py-2 text-white"
+        @click="setLocale('fr')"
+      >
+        fr
+      </button>
+      <p>{{ $t("welcome") }}</p>
+    </div>
+  </div>
 </template>
