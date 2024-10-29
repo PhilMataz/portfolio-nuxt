ff68da109501ea4e0f57c89baf8a85acb46f0f80
Phillip Matambanadzo <hello@philmataz.dev>
2024-10-29 13:06:53 +0000

    feat(cv): initialize CV page layout with profile image
    Add initial structure for CV page with a two-column layout design. The left
    column includes a profile image with specific styling for both screen and
    print views.
    
    The layout follows A4 paper dimensions (210mm x 297mm) to ensure proper
    printing compatibility while maintaining responsive design for screen
    viewing.

--- pages/cv.vue
+++ pages/cv.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts"></script>
 
 <template>
+  <div class="flex w-full flex-col items-center">
+    <div class="flex h-[297mm] w-[210mm] border-x print:border-none">
+      <div class="flex w-[78mm] flex-col bg-zinc-900 text-slate-50">
+        <img
+          class="min-h-[100mm] object-cover mix-blend-screen"
+          src="~/assets/images/Professional.BW.png"
+          alt=""
+        />
   <div>Hello CV!</div>
+      </div>
+      <div>Phillip Matambanadzo</div>
+    </div>
+  </div>
 </template>
